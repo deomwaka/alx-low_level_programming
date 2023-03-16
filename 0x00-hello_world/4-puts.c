d4198f541133bcbad6a0df22146618a9bb8e46f3
Deogratius Mwaka <deogratiusmwaka@gmail.com>
2023-03-16 16:41:33 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #!/bin/bash
 #include <stdio.h>
+/**
+ *  * main - code to output a message as described
+ *   * Return: Always 0
+ *    */
 
 int main(void)
 {
- puts("Programming is like building a multilingual puzzle\n"); 
- return 0;
+		puts("Programming is like building a multilingual puzzle\n");
+			return (0);
 }
