88062c5ab4c201449edb6a4e423b2d2812601047
Deogratius Mwaka <deogratiusmwaka@gmail.com>
2023-03-21 13:38:34 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet - entry point
+ *
+ * Description: A function that prints alphabet,
+ * in lowercase
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= z;)
+	{
+		_putchar ('c');
+		c++;
+	}
+	_putchar('\n');
+}
+
