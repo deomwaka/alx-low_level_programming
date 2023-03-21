c46e1a5ae95fac20b422647028a17a8d4840c962
Deogratius Mwaka <deogratiusmwaka@gmail.com>
2023-03-21 14:00:57 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,12 +9,12 @@
  */
 void print_alphabet(void)
 {
-	char letter = 'a';
+	char c = 'a';
 
-	while (letter <= 'z')
+	while (c <= 'z')
 	{
-		_putchar ('letter');
-		letter++;
+		_putchar ('c');
+		c++;
 	}
 	_putchar('\n');
 }
