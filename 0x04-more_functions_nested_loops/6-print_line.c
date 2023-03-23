71a25a0acd264acd544cabeea0732b25c21d603f
Deogratius Mwaka <deogratiusmwaka@gmail.com>
2023-03-23 13:52:48 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_line - prints a line in the terminal
+ * @n: integer variable
+ * Return: void
+ */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		int i;
+
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
