815bfc5fcd813b7c26043cf45923285dea5e052e
Deogratius Mwaka <deogratiusmwaka@gmail.com>
2023-03-28 10:50:53 -0700

    Task 11 #advanced

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 10
+
+int main(void)
+{
+    char password[PASSWORD_LENGTH + 1];
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    srand(time(NULL)); /* seed the random number generator with the current time */
+
+    for (int i = 0; i < PASSWORD_LENGTH; i++)
+    {
+        int index = rand() % sizeof(charset); /* generate a random index into the charset */
+        password[i] = charset[index];
+    }
+
+    password[PASSWORD_LENGTH] = '\0'; /* null-terminate the string */
+
+    printf("Random password: %s\n", password);
+
+    return (0);
+}
