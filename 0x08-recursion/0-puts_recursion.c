15309dd32fd564738dc9bce6807a7ad95f435894
Deogratius Mwaka <deogratiusmwaka@gmail.com>
2023-04-06 07:40:43 -0700

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts_recursion - print a string
+ * @s: string to be printed
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_Puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
