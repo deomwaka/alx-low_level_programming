caeb08834dc102fa598dc8921f3eeb4fa1d2afc1
Deogratius Mwaka <deogratiusmwaka@gmail.com>
2023-04-06 08:51:46 -0700

    Task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main"
+/**
+ * factorial - return factorial of a number
+ * @n: - integer to be checked
+ * Return: if n > 0 return factorial of that integer
+ *         if n < 0 return 1 to show error
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n > 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
