3ce81c123188dd0d842562e5c9ecb4f459807b26
Deogratius Mwaka <deogratiusmwaka@gmail.com>
2023-04-06 13:45:58 -0700

    Task 0

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * FILE - holberton.h
+ * Description: Header file with prototypes for all functions needed
+ */
+
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
