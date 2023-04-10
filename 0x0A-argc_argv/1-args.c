ada93993553396676a79fa15a87fea4dee2c766c
Deogratius Mwaka <deogratiusmwaka@gmail.com>
2023-04-10 11:29:24 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print number of arguments passed into it
+ * @argc: count of arguments
+ * @argv: array of arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
