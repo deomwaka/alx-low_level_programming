3cf3eed32fa8b501c0dd621c6cc36707dd5b5881
Deogratius Mwaka <deogratiusmwaka@gmail.com>
2023-04-18 07:05:20 -0700

    Task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees a memory block for dog_t
+ * @d: address pointer to struct dog_t to be freed
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
