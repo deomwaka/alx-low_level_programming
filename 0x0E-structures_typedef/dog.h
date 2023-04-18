bb058293f3709c60024f2e37b7489d6b76a3f5bc
Deogratius Mwaka <deogratiusmwaka@gmail.com>
2023-04-18 04:40:24 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new type of data represents dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
