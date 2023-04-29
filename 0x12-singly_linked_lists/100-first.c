2f9de28d480c0e2a375a75e3fe7fb410ff79ddae
deomwaka <deogratiusmwaka@gmail.com>
2023-04-29 05:10:33 -0700

    pre-main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-void first(void)__attribute__((constructor));
-
 /**
- * first - prints a sentence before the main function executes
+ * premain - a function that executes before main
+ *
+ * Return: void
  */
 
-void first(void)
+void __attribute__((constructor)) premain()
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
