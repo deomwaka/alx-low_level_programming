6acefa789f30fea723bbcd489823b3ad853b4c24
deomwaka <deogratiusmwaka@gmail.com>
2023-04-29 05:51:45 -0700

    restored

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,22 @@
-global main
-	extern printf
+; Declare needed C  functions
+	extern	printf		; the C function to be called
 
-main:
-	mov edi, format
-	xor eax, eax
-	call printf
-	mov eax, 0
-	ret
+	section .data		; Data section, initialized variables
+msg:	db "Hello, Holberton", 0; C string needs 0
+fmt:	db "%s", 10, 0		; The printf format, "\n",'0'
 
-format: db 'Hello, Holberton\n', 0
+	section .text		; Code section.
+
+	global main		; the standard gcc entry point
+main:				; the program label for the entry point
+	push	rbp		; set up stack frame, must be aligned
+
+	mov	rdi,fmt
+	mov	rsi,msg
+	mov	rax,0		; can also be xor  rax,rax
+	call	printf		; Call C function
+
+	pop	rbp		; restore stack
+
+	mov	rax,0		; normal, no error, return value
+	ret			; return
