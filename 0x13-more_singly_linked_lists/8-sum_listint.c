8a4efc17e61e6a63416c2d6ff8fe89a3730a075f
deomwaka <deogratiusmwaka@gmail.com>
2023-05-01 06:58:52 -0700

    Task 8

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -10,12 +10,11 @@
 int sum_listint(listint_t *head)
 {
 	int sum = 0;
-	listint_t *current = head;
 
-	while (current != NULL)
+	while (head != NULL)
 	{
-		sum += current->n;
-		current = current->next;
+		sum += head->n;
+		head = head->next;
 	}
 
 	return (sum);
